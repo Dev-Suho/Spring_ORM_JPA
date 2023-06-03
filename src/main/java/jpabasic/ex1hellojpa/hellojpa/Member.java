ac1daad991027aecef5f4e06d5ae62bad73c2426
Dev-Suho <devsuho99@gmail.com>
2023-06-03 15:35:49 +0900

    TableMapping_0603

--- src/main/java/jpabasic/ex1hellojpa/hellojpa/Member.java
+++ src/main/java/jpabasic/ex1hellojpa/hellojpa/Member.java
@@ -3,8 +3,10 @@ package jpabasic.ex1hellojpa.hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "MBR")
 public class Member {
         @Id
         private Long id;
