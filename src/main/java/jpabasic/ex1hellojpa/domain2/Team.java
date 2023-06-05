d9e682d98bba09e8475f75882f75c2e58eaed2bb
Dev-Suho <devsuho99@gmail.com>
2023-06-05 18:30:54 +0900

    relation_mapping_0605

---
+++ src/main/java/jpabasic/ex1hellojpa/domain2/Team.java
@@ -0,0 +1,17 @@
+package jpabasic.ex1hellojpa.domain2;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+}
