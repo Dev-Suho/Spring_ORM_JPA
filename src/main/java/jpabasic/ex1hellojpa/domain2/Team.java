7803085035215ec7b1a944533e10eeaf2ea3934f
Dev-Suho <devsuho99@gmail.com>
2023-06-06 21:23:40 +0900

    relationMapping_0606

--- src/main/java/jpabasic/ex1hellojpa/domain2/Team.java
+++ src/main/java/jpabasic/ex1hellojpa/domain2/Team.java
@@ -1,9 +1,8 @@
 package jpabasic.ex1hellojpa.domain2;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -14,4 +13,22 @@ public class Team {
 
     private String name;
 
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
