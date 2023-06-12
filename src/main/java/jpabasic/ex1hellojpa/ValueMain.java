c66afb78b763a565e8d280d180384408e31d2398
Dev-Suho <devsuho99@gmail.com>
2023-06-12 22:06:13 +0900

    value_0612

---
+++ src/main/java/jpabasic/ex1hellojpa/ValueMain.java
@@ -0,0 +1,20 @@
+package jpabasic.ex1hellojpa;
+
+import jpabasic.ex1hellojpa.domainV5.Album;
+
+public class ValueMain {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println("a == b : " + (a == b));
+
+        Integer c = 10;
+        Integer d = 10;
+        System.out.println("c == d : " + (c == d));
+
+        Album album1 = new Album("박효신", "발라드");
+        Album album2 = new Album("박효신", "발라드");
+
+        System.out.println("album1 == album2 : " + (album1.getArtist() == album2.getArtist()));
+    }
+}
--- src/main/java/jpabasic/ex1hellojpa/domainV5/Album.java
+++ src/main/java/jpabasic/ex1hellojpa/domainV5/Album.java
@@ -9,6 +9,15 @@ public class Album extends Item {
     private String artist;
     private String etc;
 
+    public Album(String artist, String etc) {
+        this.artist = artist;
+        this.etc = etc;
+    }
+
+    public Album() {
+
+    }
+
     public String getArtist() {
         return artist;
     }
